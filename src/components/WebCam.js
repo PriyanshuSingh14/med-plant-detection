631cc370ebb500a96fe2e9e59584e7bacf251851
Priyanshu1404 <priyanshubirpur@gmail.com>
2023-09-11 12:48:34 +0530

    added capture photo and retake photo options

--- src/components/WebCam.js
+++ src/components/WebCam.js
@@ -1,10 +1,62 @@
 import Webcam from "react-webcam";
+import { useCallback, useRef, useState } from "react";
+import { Button } from "@mui/material";
+import axios from "axios";
 
 const WebCam=()=>{
+    const webcamRef = useRef(null); 
+    const [imgSrc, setImgSrc] = useState(null);
+    const retake = () => {
+        setImgSrc(null);
+      };
+
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+      }, [webcamRef]);
+
+      const sendPhoto = async (e) => {
+        e.preventDefault();
+        const formdata = new FormData();
+        formdata.append("file", imgSrc);
+    
+        await axios
+          .post("http://localhost:8000/upload", formdata)
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
     return (
-        <>
-        <Webcam height={600} width={400}></Webcam>
-        </>
+        <div >
+      {imgSrc ? (
+        <img src={imgSrc} alt="webcam" />
+      ) : (
+        <Webcam height={400} width={300} ref={webcamRef} />
+      )}
+      <div >
+        {imgSrc ? (
+            <>
+          <Button onClick={retake} variant="contained"
+            component="span"
+            color="primary"
+            sx={{ mr: 2 }}>Retake photo</Button>
+          <Button variant="contained"
+            component="span"
+            color="primary"
+            sx={{ mr: 2 }} onClick={sendPhoto}>Upload photo</Button>
+          </>
+
+        ) : (
+          <Button onClick={capture} variant="contained"
+          component="span"
+          color="primary"
+          sx={{ mr: 2 }}>Capture photo</Button>
+        )}
+      </div>
+    </div>
     );
 }
 
